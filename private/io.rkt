38042ac73433b2832cff237bf264d54ce013f9e9
Ryan Culpepper <ryanc@racket-lang.org>
2021-04-06 05:38:10 +0200

    ... io: make wrapper input port that notifies on reads

--- private/io.rkt
+++ private/io.rkt
@@ -22,19 +22,90 @@
 
 ;; ----------------------------------------
 
-(struct async-exn-input-port (in exn-box sema)
+(struct async-exn-input-port (in exn-box exn-sema)
   #:property prop:input-port (struct-field-index in))
 
 (define (async-exn-input-port-raise aip e)
   (set-box! (async-exn-input-port-exn-box aip) e)
-  (semaphore-post (async-exn-input-port-sema aip))
+  (semaphore-post (async-exn-input-port-exn-sema aip))
   (void))
 
+(define (make-async-exn-input-port in [on-consume void])
+  (define exn-box (box #f))
+  (define exn-sema (make-semaphore 0))
+  (define exn-evt
+    (wrap-evt (semaphore-peek-evt exn-sema)
+              (lambda (_) (raise (unbox exn-box)))))
+  (define name (object-name in))
+  (define peeked 0) ;; peek counts as consume, but don't double-count w/ other peek/write
+  (define (read-in buf)
+    (sync/timeout 0 exn-evt)
+    #;(start-atomic)
+    (define r (read-bytes-avail!* buf in))
+    (cond [(eqv? r 0)
+           #;(end-atomic)
+           (choice-evt (handle-evt in (lambda (_in) (read-in buf)))
+                       exn-evt)]
+          [else
+           (cond [(exact-nonnegative-integer? r)
+                  (cond [(<= r peeked)
+                         (set! peeked (- peeked r))
+                         #;(end-atomic)]
+                        [else
+                         (set! peeked 0)
+                         #;(end-atomic)
+                         (on-consume (- r peeked))])]
+                 [else #;(end-atomic) (void)])
+           r]))
+  (define (peek buf skip progress-evt)
+    (sync/timeout 0 exn-evt)
+    #;(start-atomic)
+    (define r (peek-bytes-avail!* buf skip progress-evt in))
+    (cond [(eqv? r 0)
+           #;(end-atomic)
+           (choice-evt (handle-evt in (lambda (_e) (peek buf skip progress-evt)))
+                       (handle-evt (or progress-evt never-evt) (lambda (_e) #f))
+                       exn-evt)]
+          [else
+           (cond [(exact-nonnegative-integer? r)
+                  (define new-peeked (+ skip r))
+                  (cond [(> new-peeked peeked)
+                         (define diff (- new-peeked peeked))
+                         (set! peeked new-peeked)
+                         #;(end-atomic)
+                         (on-consume diff)]
+                        [else #;(end-atomic) (void)])]
+                 [else #;(end-atomic) (void)])
+           r]))
+  (define (close)
+    (sync/timeout 0 exn-evt)
+    ;; FIXME: check for exns again after closing?
+    (begin0 (close-input-port in) (on-consume 0)))
+  (define get-progress-evt
+    (and (port-provides-progress-evts? in)
+         (lambda () (port-progress-evt in))))
+  (define (commit k progress-evt done)
+    #;(start-atomic) ;; FIXME: okay to call port-commit-peeked in atomic mode?
+    (if (port-commit-peeked k progress-evt done in)
+        (begin (set! peeked (max 0 (- peeked k))) #;(end-atomic) #t)
+        (begin #;(end-atomic) #f)))
+  (define (get-location)
+    (port-next-location in))
+  (define (count-lines!)
+    (port-count-lines! in))
+  (async-exn-input-port
+   (make-input-port name read-in peek close
+                    get-progress-evt commit
+                    get-location count-lines!)
+   exn-box
+   exn-sema))
+
+#;
 (define (make-async-exn-input-port in)
   (define exn-box (box #f))
-  (define sema (make-semaphore 0))
+  (define exn-sema (make-semaphore 0))
   (define exn-evt
-    (wrap-evt (semaphore-peek-evt sema)
+    (wrap-evt (semaphore-peek-evt exn-sema)
               (lambda (_) (raise (unbox exn-box)))))
   (define name (object-name in))
   (define (read-in buf)
@@ -70,7 +141,7 @@
                     get-progress-evt commit
                     get-location count-lines!)
    exn-box
-   sema))
+   exn-sema))
 
 ;; ----------------------------------------
 
