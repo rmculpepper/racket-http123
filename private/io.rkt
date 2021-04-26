ab4bae72109e3bc21560dc75c27cf394e8696770
Ryan Culpepper <ryanc@racket-lang.org>
2021-04-26 11:24:49 +0200

    rewrite box-evt, improve update feedback

--- private/io.rkt
+++ private/io.rkt
@@ -95,28 +95,38 @@
 
 ;; ============================================================
 
-(struct box-evt (b sema evt)
+;; BoxEvt[X] = (box-evt (box (U X #f)) Semaphore Semaphore Evt[X])
+(struct box-evt (b wsema rsema evt)
   #:property prop:evt (struct-field-index evt))
-(define box-is-unset (gensym))
 
 (define (make-box-evt)
-  (define b (box box-is-unset))
-  (define sema (make-semaphore 0))
+  (define b (box #f))
+  (define wsema (make-semaphore 1))
+  (define rsema (make-semaphore 0))
   (define evt ;; FIXME: wrap or handle?
-    (wrap-evt (semaphore-peek-evt sema)
+    (wrap-evt (semaphore-peek-evt rsema)
               (lambda (_e) (unbox b))))
-  (box-evt b sema evt))
+  (box-evt b wsema rsema evt))
 
+;; box-evt-set! : BoxEvt X -> Boolean
+;; Returns #t if we set, #f if already set.
+;; Thread-safe and break-safe but not kill-safe.
 (define (box-evt-set! be v)
-  (match-define (box-evt b sema evt) be)
-  (cond [(eq? (unbox b) box-is-unset)
-         (set-box! (box-evt-b be) v)
-         (void (semaphore-post (box-evt-sema be)))
+  (match-define (box-evt b wsema rsema evt) be)
+  (define be? (break-enabled))
+  (break-enabled #f)
+  (cond [(semaphore-try-wait? wsema)
+         ;; Consumed wsema; don't post to wsema when done.
+         (set-box! b v)
+         (semaphore-post rsema)
+         (break-enabled be?)
          #t]
-        [else #f]))
+        [else
+         (break-enabled be?)
+         #f]))
 
 (define (box-evt-ready? be)
-  (not (eq? (unbox (box-evt-b be)) box-is-unset)))
+  (and (sync/timeout (semaphore-peek-evt (box-evt-rsema be))) #t))
 
 ;; ----------------------------------------
 
