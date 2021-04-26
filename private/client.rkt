953f344c4680d252068e442c6da7847c313bea3d
Ryan Culpepper <ryanc@racket-lang.org>
2021-04-26 11:56:51 +0200

    client: delete stubs of GET, etc methods

--- private/client.rkt
+++ private/client.rkt
@@ -16,6 +16,10 @@
          "util.rkt")
 (provide (all-defined-out))
 
+(define default-base-header
+  '((#"accept-encoding" #"gzip, deflate")
+    (#"user-agent" #"racket-http123/0.1")))
+
 ;; ============================================================
 
 (define response/c (is-a?/c http-response<%>))
@@ -23,33 +27,11 @@
 (define header/c (is-a?/c header<%>))
 (define data/c (or/c #f bytes? (-> (-> bytes? any) any)))
 
-(define data-method/c
-  (->*m [loc/c]
-        [#:header header/c
-         #:data data/c]
-        any))
-(define nodata-method/c
-  (->*m [loc/c]
-        [#:header header/c]
-        any))
-
 (define http-client<%>
   (interface (http-client-base<%>)
     [sync-request
      (->m request? response/c)]
-    [GET data-method/c]
-    [HEAD nodata-method/c]
-    [POST data-method/c]
-    [PUT data-method/c]
-    [PATCH data-method/c]
-    [DELETE nodata-method/c]
-    [OPTIONS nodata-method/c]
-    [TRACE nodata-method/c]
     ))
-
-(define default-base-header
-  '((#"accept-encoding" #"gzip, deflate")
-    (#"user-agent" #"racket-http123/0.1")))
 
 ;; ============================================================
 
@@ -76,27 +58,6 @@
 
     (define/public (sync-request req)
       ((sync (async-request req))))
-
-    ;; ----------------------------------------
-    ;; Reference: https://tools.ietf.org/html/rfc7231
-
-    (define-syntax-rule (make-method method)
-      (lambda (loc #:header [header null] #:data [data #f])
-        (with-entry-point 'method
-          (sync-request (request 'method loc header data)))))
-    (define-syntax-rule (make-method/no-data method)
-      (lambda (loc #:header [header null])
-        (with-entry-point 'method
-          (sync-request (request 'method loc header #f)))))
-
-    (define/public GET (make-method 'GET))
-    (define/public HEAD (make-method/no-data 'HEAD))
-    (define/public POST (make-method 'POST))
-    (define/public PUT (make-method 'PUT))
-    (define/public PATCH (make-method 'PATCH))
-    (define/public DELETE (make-method/no-data 'DELETE))
-    (define/public OPTIONS (make-method/no-data 'OPTIONS))
-    (define/public TRACE (make-method/no-data 'TRACE))
     ))
 
 #;
