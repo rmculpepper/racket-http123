e99ccac8e015245b951f90a37d34ee6b6c6c0b22
Ryan Culpepper <ryanc@racket-lang.org>
2025-03-18 10:23:25 -0400

    update test for web changes

--- http123/tests/test-web.rkt
+++ http123/tests/test-web.rkt
@@ -18,30 +18,61 @@
                #:add-content-handlers
                `([application/json ,read-json])))
 
-(test-case "data.jsontest.com"
-  (define time-req (request 'GET "http://date.jsontest.com/"))
-  (define result (send client handle time-req))
-  (check-pred jsexpr? result))
+(test-case "OpenLibrary"
+  ;; eg https://openlibrary.org/books/OL27372351M/How_to_Design_Programs
+  (define books '(("OL27372351M" "How to Design Programs")
+                  ("OL981817M" "LISP in small pieces")
+                  ("OL1547287M" "Compiling with continuations")
+                  ("OL9468435M" "Types and Programming Languages")))
+  (define prefix "https://openlibrary.org/books/")
+  (define resultv (make-vector (length books)))
+  (define work-threads
+    (for/list ([index (in-naturals)] [entry (in-list books)])
+      (define req (request 'GET (string-append prefix (car entry) ".json")))
+      (thread (lambda () (vector-set! resultv index (send client handle req))))))
+  (for-each thread-wait work-threads)
+  (for ([result (in-vector resultv)] [entry (in-list books)])
+    (check-pred jsexpr? result)
+    (check-equal? (hash-ref result 'title #f) (cadr entry))))
 
-(test-case "fail ietf.org"
+(test-case "xkcd"
+  ;; https://xkcd.com/json.html
+  (define comics '((327 "Exploits of a Mom")
+                   (297 "Lisp Cycles")
+                   (208 "Regular Expressions")
+                   (303 "Compiling")
+                   (2347 "Dependency")))
+  (define resultv (make-vector (length comics)))
+  (define work-threads
+    (for/list ([index (in-naturals)] [entry (in-list comics)])
+      (define req (request 'GET (format "https://xkcd.com/~a/info.0.json" (car entry))))
+      (thread (lambda () (vector-set! resultv index (send client handle req))))))
+  (for-each thread-wait work-threads)
+  (for ([result (in-vector resultv)] [entry (in-list comics)])
+    (check-pred jsexpr? result)
+    (check-equal? (hash-ref result 'title #f) (cadr entry))))
+
+;; Currently (3/2025), tools.ietf.org redirects to www.rfc-editor.org
+;; https://tools.ietf.org/rfc/rfc7540.txt
+;; https://www.rfc-editor.org/rfc/rfc7540.txt
+(define RFC-URL "https://www.rfc-editor.org/rfc/rfc7540.txt")
+
+(test-case "fail rfc-editor.org"
   (check-exn #rx"no content handler matched"
-             (lambda ()
-               (send client handle (request 'GET "https://tools.ietf.org/rfc/rfc7540.txt")))))
+             (lambda () (send client handle (request 'GET RFC-URL)))))
 
 (define client2
   (send client fork
         #:add-content-handlers
         `([text/plain ,(lambda (in) (read-string 40 in))]
-          [*/* ,(lambda (in)
-                  (format "something called ~s, I guess"
-                          (send (current-response) get-content-type)))])))
+          [*/* ,(lambda (in) (list (send (current-response) get-content-type)))])))
 
 (test-case "ok ietf"
-  (check-pred string?
-              (send client2 handle (request 'GET "https://tools.ietf.org/rfc/rfc7540.txt"))))
+  (check-pred string? (send client2 handle (request 'GET RFC-URL))))
+
 (test-case "ok google"
-  (check-regexp-match #rx"something called text/html, I guess"
-                      (send client2 handle (request 'GET "https://www.google.com/"))))
+  (check-equal? (send client2 handle (request 'GET "https://www.google.com/"))
+                '(text/html)))
 
 (test-case "fail mirror"
   (check-exn #rx"no response handler matched"
